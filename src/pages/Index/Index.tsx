8b128aa8a3a78059b3e6b56e71026b5a531b33bb
wzm <2791063637@qq.com>
2025-03-12 13:58:50 +0800

    feat: Implement user management functionality with search and freeze options

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,5 +1,15 @@
+import { UserOutlined } from "@ant-design/icons";
 import { Outlet } from "react-router-dom";
+import './index.css';
 
 export function Index() {
-    return <div>Index<Outlet></Outlet></div>
+    return <div id="index-container">
+        <div className="header">
+            <h1>会议室预定系统-后台管理</h1>
+            <UserOutlined className="icon"/>
+        </div>
+        <div className="body">
+            <Outlet></Outlet>
+        </div>
+    </div>
 }
