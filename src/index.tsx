c0803d08bb4d0c51b591b815647d1528973ff874
wzm <2791063637@qq.com>
2025-03-12 11:02:22 +0800

    build: Add routing and basic page components for Index, Error, User Management, and Login

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter, Link, Outlet } from 'react-router-dom';
+import { Index } from './pages/Index/Index';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+import { UserManage } from './pages/UserManage/UserManage';
+import { Login } from './pages/Login/Login';
+
+const routes = [
+    {
+        path: "/",
+        element: <Index></Index>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'user_manage',
+                element: <UserManage/>
+            }
+        ]
+    },
+    {
+        path: "login",
+        element: <Login />,
+    }
+];
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router}/>);
